32a35a930410b021805422146bd29e42340fa04c
Gustavo Alexandre Nobre Mesquita <gnobremesquita@gmail.com>
2022-02-07 00:16:07 -0300

    Initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,14 @@
+/**
+ * @type {import("@jest/types").Config.DefaultOptions}
+ */
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest',
+    '\\.js$': '<rootDir>/node_modules/babel-jest',
   },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node",
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
 };
